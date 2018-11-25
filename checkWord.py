855f72e844012c477297a0857bcbac9fbfb6a88c
matthewssullivan <42194969+matthewssullivan@users.noreply.github.com>
2018-11-24 20:45:47 -0700

    Update checkWord.py

--- checkWord.py
+++ checkWord.py
@@ -1,17 +1,18 @@
 import string
 import re
-file = open("wordlist.txt")
-words = file.read()
-for item in words:
-    item = item.lower()
-    item = re.sub(r'[^\w\s]', '', item)
 
-searchWord = input("please enter words to check")
-searchWord=searchWord.split()
-refinedList=[]
+def wordCheck(searchWord):
+    file = open("wordlist.txt")
+    words = file.read()
+    for item in words:
+        item = item.lower()
+        item = re.sub(r'[^\w\s]', '', item)
 
-for word in searchWord:
-    word = re.sub(r'[^\w\s]','',word)
-    word = word.lower() + "\n"
-    if word not in words:
-        refinedList.append(word)
+    searchWord=searchWord.split()
+    refinedList=[]
+
+    for word in searchWord:
+        word = re.sub(r'[^\w\s]','',word)
+        word = word.lower() + "\n"
+        if word not in words:
+            refinedList.append(word)
