01bbbe844cbd7ea7831a614d019f9cca662ac879
jontrejo5 <jtrejo0515@gmail.com>
2018-11-26 15:44:53 -0700

    Description from database is given

--- returnDescription.py
+++ returnDescription.py
@@ -2,12 +2,18 @@ import csv
 
 def returnDescription(input):
 
+    stringout = ''
 
     with open('allitems.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
-
-            print(f'\t{row[2]}')
+            if input[0] == row[2]:
+                 stringout += row[2]
             line_count += 1
         print(f'Processed {line_count} lines.')
+
+    if stringout == '':
+        stringout = "\nNo data found"
+
+    return stringout
