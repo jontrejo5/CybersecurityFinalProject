2b93107565d32f9c80e70fbe0e6c338394ca832a
jontrejo5 <jtrejo0515@gmail.com>
2018-11-26 15:07:03 -0700

    update return description

--- returnDescription.py
+++ returnDescription.py
@@ -7,10 +7,7 @@ def returnDescription(input):
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
-                line_count += 1
-            else:
-                print(f'\t{row[2]}')
-                line_count += 1
+
+            print(f'\t{row[2]}')
+            line_count += 1
         print(f'Processed {line_count} lines.')
