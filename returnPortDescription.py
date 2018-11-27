5a895ac4714793788793ec86f0a9427caf959673
matthewssullivan <42194969+matthewssullivan@users.noreply.github.com>
2018-11-27 01:29:28 -0700

    Update returnPortDescription.py

--- returnPortDescription.py
+++ returnPortDescription.py
@@ -1,25 +1,22 @@
+#CSCI 5742
+#Cybersecurity Programming
+#Final Project
+#Portscanner interface with CVE
+#Jonathan Trejo and Matt Sullivan
+#11/27/2018
+#returnPortDescription.py
 
 import csv
 
-def returnPortDescription(portNum):
-    with open("service-names-port-numbers (1).csv", mode = 'r') as f:
-        csvDict=csv.DictReader(f)
-        for row in csvDict:
-            workNums=row["Port Number"].split("-")
-            if len(workNums) <2:
-                if portNum == int(row["Port Number"]):
+def returnPortDescription(portNum):                     #function to return the description from the list of ports
+    with open("service-names-port-numbers (1).csv", mode = 'r') as f:   #opens the file
+        csvDict=csv.DictReader(f)                                       #reads it in as a dictionary
+        for row in csvDict:         #step through the file
+            workNums=row["Port Number"].split("-")      #checks if it's a range
+            if len(workNums) <2:                        #if it is not
+                if portNum == int(row["Port Number"]):  #return the description
                     return row["Description"]
-            else:
-                if int(workNums[0]) <= int(portNum) <= int(workNums[1]):
-                    return str(row["Description"])
-#            workLine = line.split(",")
-#            if len(workLine) >=3:
-#                if workLine[1] == portNum:
-#                    writeline = workLine[3]
-#                    return writeline
-#            else:
-#                portRange = workLine[0].split("-")
-#                if portNum >= int(portRange[0]) and portNum <= int(portRange[1]):
-#                    return workLine[1]
-
+            else:                                                   #if it is a range
+                if int(workNums[0]) <= portNum <= int(workNums[1]): #check and see if it's in range
+                    return row["Description"]                       #return the description
     f.close()
