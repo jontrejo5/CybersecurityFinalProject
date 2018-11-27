e4313e27502fc63725b06b207f08bb7c9b51cabb
matthewssullivan <42194969+matthewssullivan@users.noreply.github.com>
2018-11-27 01:28:32 -0700

    Update wordCheck.py

--- wordCheck.py
+++ wordCheck.py
@@ -1,20 +1,27 @@
-import string
-import re
+#CSCI 5742
+#Cybersecurity Programming
+#Final Project
+#Portscanner interface with CVE
+#Jonathan Trejo and Matt Sullivan
+#11/27/2018
+#wordCheck.py
 
-def wordCheck(searchWord):
-    file = open("wordlist.txt")
-    words = file.read()
+
+import string
+
+def wordCheck(searchWord):                      #Function to check a word list against a dictionary and remove words that are in the dictionary
+    file = open("wordlist.txt")                 #dictionary retrieved online
+    words = file.read()                         #reads into variable
     for item in words:
-        item = item.lower()
-#        item = re.sub(r'[^\w\s]', '', item)
+        item = item.lower()                     #converts all letters to lowercase for spell checking
 
     searchWord=searchWord.split()
     refinedList=[]
 
     for word in searchWord:
- #       word = re.sub(r'[^\w\s]','',word)
-        testword = word.lower()+"\n"
-#        word = word.lower() + "\n"
+        testword = word.lower()+"\n"        #adds a newline to the end of the word being tested because the dictionary is newline deliniated
         if testword not in words:
-            refinedList.append(word+" ")
+            refinedList.append(word+" ")    #adds a space because the strings we will be comparing the results to are whitespace deliniated
+    file.close()
+
     return refinedList
